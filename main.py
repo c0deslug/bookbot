c75f21715cf85daf36d9ebcbb0e11c902f62fb46
c0deslug <s.rados37@gmail.com>
2025-04-23 12:42:18 +0200

    added initial text ingestion

---
+++ main.py
@@ -0,0 +1,11 @@
+
+def get_book_text(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+    return file_contents
+
+def main():
+    booktext = get_book_text("/Users/srados/workspace/github.com/c0deslug/bookbot/books/frankenstein.txt")
+    print (booktext)
+
+main()
