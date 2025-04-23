245ae485e70ba454bc32304a462f68295e39228c
c0deslug <s.rados37@gmail.com>
2025-04-23 12:59:56 +0200

    removed book printing, added word counter instead

--- main.py
+++ main.py
@@ -4,8 +4,18 @@ def get_book_text(filepath):
         file_contents = f.read()
     return file_contents
 
+def wordcount(booktext):
+    words = [] # array
+    words = booktext.split() #splits the word https://docs.python.org/3.3/library/stdtypes.html?highlight=split#str.split
+    wordcount = len(words)
+    return wordcount
+    
+
 def main():
     booktext = get_book_text("/Users/srados/workspace/github.com/c0deslug/bookbot/books/frankenstein.txt")
-    print (booktext)
+    #print (booktext)
+    num_words = wordcount(booktext)
+    print (f"{num_words} words found in the document")
 
 main()
+
