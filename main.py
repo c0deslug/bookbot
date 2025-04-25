b96313a2fe62eca9ff8c9b0c86edbf84fceb4fab
c0deslug <s.rados37@gmail.com>
2025-04-25 09:03:02 +0200

    feat: Added character counting function

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 from stats import wordcount
+from stats import allchars
+from stats import numchars
 
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
     return file_contents
     
-
 def main():
     booktext = get_book_text("/Users/srados/workspace/github.com/c0deslug/bookbot/books/frankenstein.txt")
     #print (booktext)
     num_words = wordcount(booktext)
     print (f"{num_words} words found in the document")
+    unique_chars = allchars(booktext)
+    #print (unique_chars)
+    char_count = numchars(booktext, unique_chars)
+    print (char_count)
 
 main()
 
--- stats.py
+++ stats.py
@@ -4,3 +4,20 @@ def wordcount(booktext):
     wordcount = len(words)
     return wordcount
 
+def allchars(booktext):
+    booktext = booktext.lower() # turns everything into lower case
+    unique_chars = set()
+    for char in booktext:
+        unique_chars.add(char)
+    unique_chars = sorted(unique_chars) # kind of alphabetizes them https://docs.python.org/3/library/functions.html#sorted
+    return unique_chars
+    
+def numchars(booktext, unique_chars):
+    char_table = {}
+    booktext = booktext.lower()
+    for char in unique_chars: # sets the initial value of each character to zero
+        char_table[char] = 0
+    for char in booktext: # go through each character in book text and if found in the dictionary increment value
+        if char in char_table:
+            char_table[char] += 1
+    return char_table
